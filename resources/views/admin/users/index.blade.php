9d5d4d1f32af27e76a52e5be4fa783de752afa9e
Eduardo-SNantos <eduardoshw123@gmail.com>
2024-11-21 23:01:25 -0300

    criação do campo de exibição dos users

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Usuáios</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>-</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="3">Nenhum usuário no banco de dados</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+    {{ $users->links() }}
+</body>
+</html>
